2ccca9237999bfb317933ae2471757d821723302
zeriong <jaeryong95@gmail.com>
2025-08-05 16:25:34 +0900

    feat: prettier 추가적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
 import EasyLexicalEditor from "./components/EasyLexicalEditor.tsx";
 
 function App() {
-    const [contentList, setContentList] = useState<any[]>([])
+  const [contentList, setContentList] = useState<any[]>([]);
+
+  useEffect(() => {}, []);
 
   return (
     <div className={"easy_lexical_test_container"}>
-        <div className={"easy_lexical_test_inner"}>
-            <p className={"easy_lexical_test_title"}>Hello!</p>
+      <div className={"easy_lexical_test_inner"}>
+        <p className={"easy_lexical_test_title"}>Hello!</p>
 
-            {contentList?.map((content) => {
-                return <div dangerouslySetInnerHTML={{ __html: content }} />
-            })}
-        </div>
+        <EasyLexicalEditor />
 
-        <EasyLexicalEditor/>
+        {contentList?.map((content) => {
+          return <div dangerouslySetInnerHTML={{ __html: content }} />;
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
